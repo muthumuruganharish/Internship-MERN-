12654d18c2ad5a855076a06f15354602ec5036ab
harish <muthumuruganharish272003@gmail.com>
2025-07-05 09:07:37 +0530

    Initial commit

---
+++ Js basics/String.js
@@ -0,0 +1,13 @@
+let value1=["a","b","c","d"]
+let value2=[7,27,37,47]
+
+console.log()
+
+for(let i=0;i<value1.length;i++){
+    console.log(value1[i])
+}
+let j=0
+while(j<value2.length){
+    console.log(value2[j])
+    j+=1;
+}
